3551e74cad82c9b07198197f4ce61dde97918e67
huzaifalimbadiya <hujjuhujju729@gmail.com>
2026-01-18 23:10:32 +0530

    Register models in admin

--- app/apps.py
+++ app/apps.py
@@ -1,17 +1,5 @@
 from django.apps import AppConfig
 
-
 class AppConfigCustom(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
-
-    def ready(self):
-        from .models import AdminUser
-
-        user, created = AdminUser.objects.get_or_create(
-            username="huzaifa",
-            defaults={"is_active": True}
-        )
-        user.set_password("Huzaifa")
-        user.is_active = True
-        user.save()
