fa018264c0d902dc92eda9da2bba114149479785
huzaifalimbadiya <hujjuhujju729@gmail.com>
2026-01-18 20:34:58 +0530

    Auto create admin user on startup

--- app/apps.py
+++ app/apps.py
@@ -4,3 +4,22 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+
+
+from django.apps import AppConfig
+
+
+class AppConfigCustom(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app'
+
+    def ready(self):
+        from .models import AdminUser
+
+        if not AdminUser.objects.filter(username="admin").exists():
+            AdminUser.objects.create(
+                username="admin",
+                password="12345",
+                is_active=True
+            )
