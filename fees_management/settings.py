7ff70e42bc859e6f94a1d35e2c5dd8dc46499b04
huzaifalimbadiya <hujjuhujju729@gmail.com>
2026-01-18 21:03:40 +0530

    Connect postgres database

--- fees_management/settings.py
+++ fees_management/settings.py
@@ -1,3 +1,6 @@
+import os
+import dj_database_url
+
 """
 Django settings for fees_management project.
 
@@ -77,11 +80,11 @@ WSGI_APPLICATION = 'fees_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
