3ea7b3b2e32fd4cee000b3e279061be5de2d91a1
huzaifalimbadiya <hujjuhujju729@gmail.com>
2026-01-18 22:47:22 +0530

    Fix admin password hashing

--- app/apps.py
+++ app/apps.py
@@ -1,14 +1,13 @@
 from django.apps import AppConfig
 
-
 class AppConfigCustom(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
 
     def ready(self):
-      from .models import AdminUser
+        from .models import AdminUser
 
-      if not AdminUser.objects.filter(username="huzaifa").exists():
-        user = AdminUser(username="huzaifa", is_active=True)
-        user.set_password("Huzaifa")   # ✅ password hash hoga
-        user.save()
+        if not AdminUser.objects.filter(username="huzaifa").exists():
+            user = AdminUser(username="huzaifa", is_active=True)
+            user.set_password("Huzaifa")
+            user.save()
