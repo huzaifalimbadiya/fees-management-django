1e2d3735122f6a4516d417ba6bacea854e8883f2
huzaifalimbadiya <hujjuhujju729@gmail.com>
2026-01-18 23:01:50 +0530

    Fix admin password hashing

--- app/apps.py
+++ app/apps.py
@@ -1,11 +1,17 @@
-def ready(self):
-    from .models import AdminUser
+from django.apps import AppConfig
 
-    user, created = AdminUser.objects.get_or_create(
-        username="huzaifa",
-        defaults={"is_active": True}
-    )
 
-    user.set_password("huzaifa")   # 🔥 hamesha password update hoga
-    user.is_active = True
-    user.save()
+class AppConfigCustom(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app'
+
+    def ready(self):
+        from .models import AdminUser
+
+        user, created = AdminUser.objects.get_or_create(
+            username="huzaifa",
+            defaults={"is_active": True}
+        )
+        user.set_password("Huzaifa")
+        user.is_active = True
+        user.save()
